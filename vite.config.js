58c5db79526558b509e710c0c7dfa75877cafc97
Ben <105577567+benjbright@users.noreply.github.com>
2022-09-22 09:40:57 +0100

    Update for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/2_buy_me_a_coffee/",
+  base: "/2_buy_me_a_coffee/",
   plugins: [react()],
 })
